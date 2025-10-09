977a92ba4aa65f6d8b760144027ba377c53aca3b
Cameron Lee <7cameronlee@gmail.com>
2025-10-08 21:00:20 -0700

    fix

--- proj2/modules/hello/lkmhello.c
+++ proj2/modules/hello/lkmhello.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
   printf("Hello world! Group11 in kernel space\n");
-=======
-  printf("Hello world! Group11 in kernel space\n");
->>>>>>> d789d3acfa7d45248af9d4994e6c959696a62231
-
   return 0;
 }
