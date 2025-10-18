5d2e8a9b8bb17a00c956f2abc2a4701cc9137f86
jakedlrSDSU <jdelosreyes8968@sdsu.edu>
2025-10-17 17:47:44 -0700

    Added mod_calc.c, changed sys_calc.c so it works with new mod_calc.c, made minor change to mod_calc/Makefile

--- proj2/kernel/sys_calc.c
+++ proj2/kernel/sys_calc.c
@@ -1,8 +1,14 @@
 #include <linux/syscalls.h> //To modify system calls
 #include <linux/kernel.h> //Not sure how necessary this is but every example seems to have it
 #include <linux/uaccess.h> //for pointer references
+#include <linux/errno.h> //-EFAULT
+#include <linux/export.h> //EXPORT_SYMBOL
 
-SYSCALL_DEFINE4(calc, int, param1, int, param2, char, operation, int __user *, result) {
+//defining a function-pointer type so that we can later create variations of it depending on if a modules is implemented or not to be called by the calc system call
+typedef asmlinkage long (*calc_t)(int, int, char, int __user * );
+
+//default implementation
+static asmlinkage long calc_default(int param1, int param2, char operation, int __user *result) {
 	int output;
 	switch(operation) {
 		case '+':
@@ -17,6 +23,7 @@ SYSCALL_DEFINE4(calc, int, param1, int, param2, char, operation, int __user *, r
 		case '/':
 			if(param2 == 0) { //Invalid denominator
 				return -1;
+			}
 			output = param1 / param2;
 			break;
 		default: //Invalid operator
@@ -27,4 +34,12 @@ SYSCALL_DEFINE4(calc, int, param1, int, param2, char, operation, int __user *, r
 		return -EFAULT;
 	}
 	return 0; //If it makes it to this point all arguments work
+}
+
+//Set current version to the default, may be modified later by modules
+calc_t calc_curr = calc_default;
+EXPORT_SYMBOL(calc_curr);
+
+SYSCALL_DEFINE4(calc, int, param1, int, param2, char, operation, int __user *, result) {
+	return calc_curr(param1, param2, operation, result);
 }
