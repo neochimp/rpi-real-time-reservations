517fe43e963b136663e52ccc36cc0ae43ab5656e
neonetizen <justin.pelak7@gmail.com>
2025-11-06 19:27:16 -0800

    changed calib heuristic to average of 3

--- proj3/apps/dummy_task/dummy_task.c
+++ proj3/apps/dummy_task/dummy_task.c
@@ -20,47 +20,31 @@ void dummy_load(int execution_time_ms) {
 
 void calibrate() {
     // target times might need adjusting, fixable later
-    const int target_test1 = 50;
-    const int target_test2 = 100;
+    const int target = 100;
+    const int its = 3;
 
     struct timeval tv;
     
     long long start_us, end_us;
-    double actual_time1, actual_time2;
+    double total_time = 0.0;
     
     // stabilize CPU
     dummy_load(500);
     dummy_load(500);
-   
-    // start timer
-    gettimeofday(&tv, NULL);
-    start_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
+    for (int i = 0; i < its; i++) { 
+        // start timer
+        gettimeofday(&tv, NULL);
+        start_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
     
-    dummy_load(target_test1);
+        dummy_load(target);
     
-    // end timer
-    gettimeofday(&tv, NULL);
-    end_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
+        // end timer
+        gettimeofday(&tv, NULL);
+        end_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
     
-    // find actual time in milliseconds
-    actual_time1 = (end_us - start_us) / 1000.0;
-
-    gettimeofday(&tv, NULL);
-    start_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
-    
-    dummy_load(target_test2);
-    
-    gettimeofday(&tv, NULL);
-    end_us = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
-    
-    actual_time2 = (end_us - start_us) / 1000.0;
-    
-    // find slope of ms per iteration-unit
-    double slope = (actual_time2 - actual_time1) / 
-                   (dummy_load_calib * (target_test2 - target_test1));
-    
-    // inverse calibration (gives us how many loop iterations per millisecond are needed)
-    dummy_load_calib = (int)((1./slope)+0.5);
+        total_time += (end_us - start_us) / 1000.0;
+    }
+    dummy_load_calib = (int)(((double)dummy_load_calib * target / (total_time / 3)) + 0.5);
 }
 
 int main(int argc, char* argv[]) {
