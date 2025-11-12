3aa46a40f1cc598062dd251c159e71daedcec501
neonetizen <justin.pelak7@gmail.com>
2025-11-12 12:44:55 -0800

    added pseudocode and formatting for reservation.c

--- proj3/apps/dummy_task/dummy_task.c
+++ proj3/apps/dummy_task/dummy_task.c
@@ -10,12 +10,12 @@
 #define MSEC_PER_SEC 1000.0
 
 #define INNER_ITERATIONS 1000 // how many times our "nop" instruction is ran
-#define DEFAULT_CALIB 1000 // initial guess for calib
+#define DEFAULT_CALIB    1000 // initial guess for calib
 
-#define CALIB_WARMUP_MS 500 // each CPU warmup burst
-#define CALIB_WARMUP_RUNS 2 // how many warmups
-#define CALIB_TARGET_MS 100 // each measured run target
-#define CALIB_ITERATIONS 10 // how many measured runs
+#define CALIB_WARMUP_MS   500 // each CPU warmup burst
+#define CALIB_WARMUP_RUNS 2   // how many warmups
+#define CALIB_TARGET_MS   100 // each measured run target
+#define CALIB_ITERATIONS  10  // how many measured runs
 
 
 // global calibration var
